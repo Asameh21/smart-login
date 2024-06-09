5d3339daf7d010db0a2fb0badca6f688743f59eb
Asameh21 <145880952+Asameh21@users.noreply.github.com>
2024-06-09 15:06:45 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -239,19 +239,19 @@ button.addEventListener("click", (e) => {
 //   });
 // }
 
-function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
-  $("#name").text(profile.getName());
-  $("#email").text(profile.getEmail());
-  $(".data").css("display", "block");
-  $(".g-signin2").css("display", "none");
-}
+// function onSignIn(googleUser) {
+//   var profile = googleUser.getBasicProfile();
+//   $("#name").text(profile.getName());
+//   $("#email").text(profile.getEmail());
+//   $(".data").css("display", "block");
+//   $(".g-signin2").css("display", "none");
+// }
 
-function signOut() {
-  var auth2 = gapi.auth2.getAuthInstance();
-  auth2.signOut().then(function () {
-    Swal.fire("Signed out.");
-    $(".g-signin2").css("display", "block");
-    $(".data").css("display", "none");
-  });
-}
+// function signOut() {
+//   var auth2 = gapi.auth2.getAuthInstance();
+//   auth2.signOut().then(function () {
+//     Swal.fire("Signed out.");
+//     $(".g-signin2").css("display", "block");
+//     $(".data").css("display", "none");
+//   });
+// }
